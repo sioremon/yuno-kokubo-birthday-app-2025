a69eefdbf825604ced2553c4f9b0b76f1f7b9e02
sioremon <cd_sioremon@hatena.ne.jp>
2024-08-31 08:49:32 +0900

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://sioremon.github.io/instagram-birthday-event-demo-app/',
+  base: process.env.GITHUB_PAGES ? "レポジトリ名" : "./",
 })
