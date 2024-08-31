3a6ff6de26606daa81261ebc16fac884f005accc
sioremon <cd_sioremon@hatena.ne.jp>
2024-08-31 10:04:41 +0900

    test

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { BrowserRouter as Router } from "react-router-dom";
+// import { BrowserRouter as Router } from "react-router-dom";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Router basename="instagram-birthday-event-demo-app">
+    {/* <Router basename="instagram-birthday-event-demo-app"> */}
       <App />
-    </Router>
+    {/* </Router> */}
   </StrictMode>,
 )
 
