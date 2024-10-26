8757e0d1807b8b2243f263c834157d89fc25490d
sioremon <cd_sioremon@hatena.ne.jp>
2024-10-27 06:56:37 +0900

    雑にtailwindcssを当てた
    ドラッグのバウンダリーの処理を入れたときに改行しても反映されないバグが入っていたので直した

--- src/App.tsx
+++ src/App.tsx
@@ -35,6 +35,7 @@ function App() {
 
   // テキストエリアに入力されたテキストを読み取り, メッセージカードのテキストを更新する関数
   const triggerOnChangeEvent = useCallback((e: React.ChangeEvent<HTMLTextAreaElement>)=>{
+    console.log(e.target.value)
     setMessage(e.target.value)
   }, [])
     // messageの状態管理をuseEffectで行う
@@ -98,8 +99,26 @@ function App() {
 
     return <Image image={img} />
   }
-
   
+  // メッセージの改行の数をカウントする関数
+  const countLineBreaks = (text: string) => {
+    return text.split('\n').length - 1;
+  }
+
+  const lineLengthAlert = () => {
+    if (!isValidLineLength) {
+      return (
+      <div className="flex items-center p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400" role="alert">
+        <svg className="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+          <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+        </svg>
+        <span className="sr-only">Info</span>
+        <div>
+          <span className="font-medium">1行が長すぎます！</span> 1行は{LINE_LENGTH}文字以内にしてください
+        </div>
+      </div>)
+    }
+  }
 
   useEffect(() => {
     if (divRef.current){
@@ -113,102 +132,210 @@ function App() {
 
   return (
     <>
-    <div ref={divRef}>
-      <h1>中山莉子生誕企画 メッセージカード</h1>
-      <p id="proto-notice">端末によっては文字が期待通りに表示されない場合があります.(修正中)</p>
-      <p>推奨ブラウザはChromeです. Safari上では期待通りに動作しない可能性があります. </p>
-      <p>莉子ちゃんの生誕企画のメッセージカードを作成できます!</p>
-      <p>メッセージは100文字まで入力できます</p>
-      <p>1行17文字を目安に改行してください(40pxの場合)</p>
-      <p>絵文字は非推奨です. </p>
-      <p>表示された文字は画像の中で動かすことができます</p>
-      <p>メッセージを書き終わったらページ下部のダウンロードボタンを押してダウンロードしてください</p>
-      
-      <Stage 
-        width={stageSize} 
-        height={stageSize} 
-        ref={stageRef} 
-        scaleX={scale} 
-        scaleY={scale}
-      >
-          <Layer>
-          {Card()}
-          <Text
-              text={message}
-              fontSize={messageFontSize}
-              fill="black"
-              fontFamily="uzura"
-              x={messagePosition.x}
-              y={messagePosition.y}
-              width={message.length * messageFontSize}
-              height={messageFontSize}
-              draggable={true}
-              dragBoundFunc={messageDragBound}
-              onDragMove={(e) => {
-                setMessagePosition({
-                  x: e.target.x(),
-                  y: e.target.y(),
-                });
-              }}
-            />
-            <Text
-              text={name}
-              fontSize={nameFontSize}
-              fill="black"
-              fontFamily="uzura"
-              x={namePosition.x}
-              y={namePosition.y}
-              width={name.length * nameFontSize}
-              height={nameFontSize}
-              draggable={true}
-              dragBoundFunc={nameDragBound}
-              onDragMove={(e) => {
-                setNamePosition({
-                  x: e.target.x(),
-                  y: e.target.y(),
-                });
-              }}
-            />
-          </Layer>
-        </Stage>
+    <div ref={divRef} className="items-center p-4" >
+      <h1 className='text-5xl mb-6'>中山莉子生誕企画 メッセージカード</h1>
+      <div className='mb-16'>
+        <p className='text-3xl  mb-6 text-red-500'>端末によっては文字が期待通りに表示されない場合があります.(修正中)</p>
+        <p className='text-3xl mb-6'>推奨ブラウザはChromeです. Safari上では期待通りに動作しない可能性があります. </p>
+        <p className='text-3xl mb-6'>メッセージは100文字まで入力できます</p>
+        <p className='text-3xl mb-6'>1行17文字を目安に改行してください(40pxの場合)</p>
+        <p className='text-3xl mb-6'>絵文字は非推奨です. </p>
+        <p className='text-3xl mb-6'>表示された文字は画像の中で動かすことができます</p>
+        <p className='text-3xl mb-6'>メッセージを書き終わったらページ下部のダウンロードボタンを押してダウンロードしてください</p>
       </div>
-      <div className="card">
-        <h2>名前を入力してください</h2>
-        <textarea onChange={(e) => setName(e.target.value)} maxLength={45}></textarea>
-        <h2>名前のフォントサイズを選択してください</h2>
-        <select onChange={onNameFontSizeChange} value={nameFontSize}>
-          <option value="20">20px</option>
-          <option value="30">30px</option>
-          <option value="35">35px</option>
-          <option value="40">40px</option>
-          <option value="45">45px</option>
-          <option value="50">50px</option>
-          <option value="55">55px</option>
-          <option value="60">60px</option>
-          <option value="70">70px</option>
-          <option value="80">80px</option>
-          {/* <option value="90">90px</option> */}
-          {/* <option value="100">100px</option> */}
-        </select>
-        <h2>メッセージを入力してください</h2>
-        <textarea onChange={triggerOnChangeEvent} maxLength={110}></textarea>
-        {!isValidLineLength && <p id="warn">(60pxの場合)1行は{LINE_LENGTH}文字以内にしてください</p>}
-        <h2>メッセージのフォントサイズを選択してください</h2>
-        <select onChange={onMessageFontSizeChange} value={messageFontSize}>
-          <option value="40">40px</option>
-          <option value="45">45px</option>
-          <option value="50">50px</option>
-          <option value="55">55px</option>
-          <option value="60">60px</option>
-          <option value="70">70px</option>
-          <option value="80">80px</option>
-          <option value="90">90px</option>
-        </select>
-        <p><button onClick={download}>ダウンロード!</button></p>
+      <div className='mb-11'>
+        <button onClick={download} className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
+          <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
+          <span>画像をダウンロード</span>
+        </button>
+      </div>
+      <div className='mb-9'>
+        <Stage 
+          width={stageSize} 
+          height={stageSize} 
+          ref={stageRef} 
+          scaleX={scale} 
+          scaleY={scale}
+        >
+            <Layer>
+            {Card()}
+            <Text
+                text={message}
+                fontSize={messageFontSize}
+                fill="#21A0DB"
+                fontFamily="uzura"
+                x={messagePosition.x}
+                y={messagePosition.y}
+                width={message.length * messageFontSize}
+                height={messageFontSize * (countLineBreaks(message)+1)}
+                draggable={true}
+                dragBoundFunc={messageDragBound}
+                onDragMove={(e) => {
+                  setMessagePosition({
+                    x: e.target.x(),
+                    y: e.target.y(),
+                  });
+                }}
+              />
+              <Text
+                text={name}
+                fontSize={nameFontSize}
+                fill="#21A0DB"
+                fontFamily="uzura"
+                x={namePosition.x}
+                y={namePosition.y}
+                width={name.length * nameFontSize}
+                height={nameFontSize * (countLineBreaks(name)+1)}
+                draggable={true}
+                dragBoundFunc={nameDragBound}
+                onDragMove={(e) => {
+                  setNamePosition({
+                    x: e.target.x(),
+                    y: e.target.y(),
+                  });
+                }}
+              />
+            </Layer>
+          </Stage>
+        </div>
+      </div>
+
+      <div>
+        <label htmlFor="message" className="block mb-4 text-3xl font-medium text-gray-900 dark:text-white">名前を入力</label>
+        <textarea onChange={(e) => setName(e.target.value)} maxLength={45} id='nameTextArea' 
+        rows={1}
+        className="
+          mb-9
+          block 
+          p-2.5 
+          w-full 
+          text-3xl
+          text-gray-900 
+          bg-transparent 
+          rounded-lg 
+          border 
+          border-gray-300 
+          focus:ring-blue-500 
+          focus:border-blue-500 
+          dark:bg-gray-700 
+          dark:border-gray-600 
+          dark:placeholder-gray-400 
+          dark:text-white 
+          dark:focus:ring-blue-500 
+          dark:focus:border-blue-500
+        " 
+        placeholder="名前を入力"></textarea>
+
+
+        <label htmlFor="message" className="block mb-4 text-3xl font-medium text-gray-900 dark:text-white">文字サイズ</label>
+        <form className="max-w-sm mx-auto">
+          <select onChange={onNameFontSizeChange} value={nameFontSize} 
+          className="mb-24 
+          bg-gray-50 
+          border 
+          border-gray-300 
+          text-gray-900 
+          text-lg
+          rounded-lg 
+          focus:ring-blue-500 
+          focus:border-blue-500 
+          block 
+          w-full 
+          p-2.5 
+          dark:bg-gray-700 
+          dark:border-gray-600 
+          dark:placeholder-gray-400 
+          dark:text-white 
+          dark:focus:ring-blue-500 
+          dark:focus:border-blue-500
+          ">
+            <option value="20">20px</option>
+            <option value="25">25px</option>
+            <option value="30">30px</option>
+            <option value="35">35px</option>
+            <option value="40">40px</option>
+            <option value="45">45px</option>
+            <option value="50">50px</option>
+            <option value="55">55px</option>
+            <option value="60">60px</option>
+          </select>
+        </form>
+          <div>
+            <label htmlFor="message" className="block mb-6 text-3xl font-medium text-gray-900 dark:text-white">メッセージを入力</label>
+
+            <textarea onChange={triggerOnChangeEvent} maxLength={110} id='messageTextArea' rows={8}
+            className="
+              mb-9
+              block 
+              p-2.5 
+              w-full 
+              text-3xl
+              text-gray-900 
+              bg-transparent
+              rounded-lg 
+              border 
+              border-gray-300 
+              focus:ring-blue-500 
+              focus:border-blue-500 
+              dark:bg-gray-700 
+              dark:border-gray-600 
+              dark:placeholder-gray-400 
+              dark:text-white 
+              dark:focus:ring-blue-500 
+              dark:focus:border-blue-500
+            " ></textarea>
+
+            {!isValidLineLength && lineLengthAlert()}
+            
+            <label htmlFor="message" className="block mb-6 text-3xl font-medium text-gray-900 dark:text-white">文字サイズ</label>
+            <form className="max-w-sm mx-auto">
+              <select onChange={onMessageFontSizeChange} value={messageFontSize} 
+              className="
+                mb-24 
+                bg-gray-50 
+                border 
+                border-gray-300 
+                text-gray-900 
+                text-lg
+                rounded-lg 
+                focus:ring-blue-500 
+                focus:border-blue-500 
+                block 
+                w-full 
+                p-2.5 
+                dark:bg-gray-700 
+                dark:border-gray-600 
+                dark:placeholder-gray-400 
+                dark:text-white 
+                dark:focus:ring-blue-500 
+                dark:focus:border-blue-500
+              ">
+                <option value="40">40px</option>
+                <option value="45">45px</option>
+                <option value="50">50px</option>
+                <option value="55">55px</option>
+                <option value="60">60px</option>
+                <option value="65">65px</option>
+                <option value="70">70px</option>
+                <option value="75">75px</option>
+                <option value="80">80px</option>
+                <option value="85">85px</option>
+                <option value="90">90px</option>
+                <option value="95">95px</option>
+                <option value="100">100px</option>
+                <option value="105">105px</option>
+                <option value="110">110px</option>
+                <option value="115">115px</option>
+                <option value="120">120px</option>
+              </select>
+            </form>
+          </div>
+        
+        {/* <button onClick={download} className='"bg-blue-500 hover:bg-blue-700 text-black font-bold py-2 px-4 border border-blue-500 rounded-full dark:text-white"'>ダウンロード!</button> */}
       </div>
       <div>
-        <p id="notice">このページは私立恵比寿中学のファン有志によって作成されました. 私立恵比寿中学公式とは無関係です</p>
-        <p>Contact</p>
+        <p className='text-lg'>このページは私立恵比寿中学のファン有志によって作成されました. 私立恵比寿中学公式とは無関係です</p>
+        <p className='text-2xl'>Contact</p>
         <ul>
           <li>GitHub: <a href="https://github.com/sioremon">@sioremon</a></li>
           <li>Mail: <a href="mailto:sioremon@sioremon.dev">sioremon [at] sioremon.dev</a></li>
